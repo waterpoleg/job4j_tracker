84ad0013adedd459c0be2aedb4c5f85588d36537
Oleg Korneev <waterpoleg@gmail.com>
2021-11-08 16:15:12 +0300

    4. Переопределение [#174890]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,10 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String body) {
+        return "{" + System.lineSeparator()
+                + "\t\"name\" : \"Report's name\"," + System.lineSeparator()
+                + "\t\"body\" : \"Report's body\"" + System.lineSeparator()
+                + "}";
+    }
+}
