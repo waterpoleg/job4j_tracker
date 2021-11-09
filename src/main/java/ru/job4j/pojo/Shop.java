40a795d4d23b550eb66677780b27676b859b7b14
Oleg Korneev <waterpoleg@gmail.com>
2021-11-09 18:26:05 +0300

    3.1. Массив с пустыми ячейками [#176470 #226239] - refactor

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -2,13 +2,11 @@ package ru.job4j.pojo;
 
 public class Shop {
     public static int indexOfNull(Product[] products) {
-        int nullIndex = -1;
         for (int i = 0; i < products.length; i++) {
             if (products[i] == null) {
-                nullIndex = i;
-                return nullIndex;
+                return i;
             }
         }
-        return nullIndex;
+        return -1;
     }
 }
