8e2a2b5c8dbde91163dab487b1561cdb484a5ff3
Oleg Korneev <waterpoleg@gmail.com>
2021-11-10 22:49:02 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #226828] - refactor

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -59,7 +59,7 @@ public class Tracker {
         int index = indexOf(id);
         boolean result = index != -1;
         if (result) {
-            System.arraycopy(items, index + 1, items, index, size - index);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
