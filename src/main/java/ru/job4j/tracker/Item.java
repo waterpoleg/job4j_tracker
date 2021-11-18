a30823937dfed9f0a73f2aea6ca50153e09f24af
Oleg Korneev <waterpoleg@gmail.com>
2021-11-19 01:37:25 +0300

    0. Сортировка [#10096] - add override equals & hash

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public class Item implements Comparable<Item> {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.
@@ -53,5 +54,22 @@ public class Item implements Comparable<Item> {
     @Override
     public int compareTo(Item another) {
         return name.compareTo(another.name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return Objects.equals(id, item.id) && Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
