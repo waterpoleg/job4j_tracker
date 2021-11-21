594b3c4b41055a03b46e990057238263e8fc7296
Oleg Korneev <waterpoleg@gmail.com>
2021-11-21 21:36:44 +0300

    1.3. Встроенные функциональные интерфейсы. [#249209]

---
+++ src/main/java/ru/job4j/function/StrategyUsage.java
@@ -0,0 +1,21 @@
+package ru.job4j.function;
+
+import java.util.function.Predicate;
+
+public class StrategyUsage {
+    public boolean empty(String s) {
+        return check(str -> str.isEmpty(), s);
+    }
+
+    public boolean startWith(String s, String pref) {
+        return check(str -> str.startsWith(pref), s);
+    }
+
+    public boolean contains(String s, String key) {
+        return check(str -> str.contains(key), s);
+    }
+
+    public boolean check(Predicate<String> pred, String s) {
+        return pred.test(s);
+    }
+}
