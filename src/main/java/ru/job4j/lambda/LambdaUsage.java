7624924cf84ccf6ce6987de2322ab030b804c7dc
Oleg Korneev <waterpoleg@gmail.com>
2021-11-21 23:00:27 +0300

    2.2. Лямбда блок [#249208]

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,12 @@
+package ru.job4j.lambda;
+
+import java.util.Comparator;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        Comparator<String> cmp = (left, right) -> {
+            System.out.println("compare - " + left.length() + " : " + right.length());
+            return Integer.compare(right.length(), left.length());
+        };
+    }
+}
