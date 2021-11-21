c273cf07d2123de088581414e8450297f0fcdc13
Oleg Korneev <waterpoleg@gmail.com>
2021-11-21 23:19:02 +0300

    3. Ссылки на методы [#70608]

---
+++ src/main/java/ru/job4j/lambda/LazyLambdaUsage.java
@@ -0,0 +1,18 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class LazyLambdaUsage {
+    public static void main(String[] args) {
+        String[] names = {
+                "Ivan",
+                "Petr"
+        };
+        Comparator<String> lengthCmp = (left, right) -> {
+            System.out.println("execute comparator");
+            return left.length() - right.length();
+        };
+        Arrays.sort(names, lengthCmp);
+    }
+}
