597dd2c9cb04888a3220e6d6ffceaee22bff75d5
Oleg Korneev <waterpoleg@gmail.com>
2021-11-23 00:59:44 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Address.java
+++ src/main/java/ru/job4j/stream/Address.java
@@ -1,5 +1,7 @@
 package ru.job4j.stream;
 
+import java.util.Objects;
+
 public class Address {
     private String city;
     private String street;
@@ -11,6 +13,27 @@ public class Address {
         this.street = street;
         this.home = home;
         this.apartment = apartment;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Address address = (Address) o;
+        return city.equals(address.getCity());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city);
     }
 }
 
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -19,7 +19,26 @@ public class ProfilesTest {
         Profile prof3 = new Profile(addr3);
         List<Profile> profileList = List.of(prof1, prof2, prof3);
         List<Address> rsl = profiles.collect(profileList);
-        List<Address> exp = List.of(addr1, addr2, addr3);
+        List<Address> exp = List.of(addr1, addr3, addr2);
+        assertThat(rsl, is(exp));
+    }
+
+    @Test
+    public void collectDistinct() {
+        Profiles profiles = new Profiles();
+        Address addr1 = new Address("Moscow", "Red Square", 1, 1);
+        Address addr2 = new Address("St.Petersburg", "Palace Square", 10, 10);
+        Address addr3 = new Address("Novosibirsk", "Lenin Square", 100, 100);
+        Address addr4 = new Address("Novosibirsk", "Lenin Square", 100, 100);
+        Address addr5 = new Address("Novosibirsk", "Lenin Square", 100, 100);
+        Profile prof1 = new Profile(addr1);
+        Profile prof2 = new Profile(addr2);
+        Profile prof3 = new Profile(addr3);
+        Profile prof4 = new Profile(addr4);
+        Profile prof5 = new Profile(addr5);
+        List<Profile> profileList = List.of(prof1, prof2, prof3, prof4, prof5);
+        List<Address> rsl = profiles.collect(profileList);
+        List<Address> exp = List.of(addr1, addr3, addr2);
         assertThat(rsl, is(exp));
     }
 }
