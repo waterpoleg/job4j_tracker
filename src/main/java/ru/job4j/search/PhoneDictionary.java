4f3745e4632227f8b9896f3a4ae6220e0a065953
Oleg Korneev <waterpoleg@gmail.com>
2021-11-24 15:23:36 +0300

    2. Local-Variable Type Inference [#70626]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -16,13 +16,13 @@ public class PhoneDictionary {
      * @return Список подошедщих пользователей.
      */
     public ArrayList<Person> find(String key) {
-        ArrayList<Person> result = new ArrayList<>();
+        var result = new ArrayList<Person>();
         Predicate<Person> checkName = person -> person.getName().contains(key);
         Predicate<Person> checkSurname = person -> person.getSurname().contains(key);
         Predicate<Person> checkPhone = person -> person.getPhone().contains(key);
         Predicate<Person> checkAddress = person -> person.getAddress().contains(key);
-        Predicate<Person> combine = checkName.or(checkSurname.or(checkPhone.or(checkAddress)));
-        for (Person person : persons) {
+        var combine = checkName.or(checkSurname.or(checkPhone.or(checkAddress)));
+        for (var person : persons) {
             if (combine.test(person)) {
                 result.add(person);
             }
--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -10,21 +10,21 @@ import static org.junit.Assert.assertTrue;
 public class PhoneDictionaryTest {
     @Test
     public void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Petr");
+        var persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
 
     @Test
     public void whenDoesntFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Martin");
+        var persons = phones.find("Martin");
         assertTrue(persons.isEmpty());
     }
 }
