75916031bee1f5ba0682a9fdbcf575cbd1889bbe
Oleg Korneev <waterpoleg@gmail.com>
2022-02-09 13:11:06 +0300

    1. Эксперименты с различными GC [#1590]

---
+++ src/main/java/ru/job4j/tracker/GC_test.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker;
+
+public class GC_test {
+    public static void main(String[] args) {
+        double start;
+        double finish;
+        MemTracker memTracker = new MemTracker();
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 100_000; i++) {
+            memTracker.add(new Item("item #" + i));
+        }
+        finish = System.currentTimeMillis();
+        System.out.printf("%nApp running time: %f%n%n", (finish - start ) / 1000);
+    }
+}
